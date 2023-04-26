2e13dcd36da2c1be20a957afd2416f4d1e19f2b0
Diego-Ulpin12 <diego.ulpin@alumnos.upla.cl>
2023-04-26 15:41:37 -0400

    Clase 26/4/23, Periodo 1

---
+++ Operadores Aritmeticos.py
@@ -0,0 +1,22 @@
+print("Suma")
+print(2 + 3)
+
+print("Resta")
+print(5 - 2)
+
+print("Multiplicacion")
+print(5 * 2)
+
+print("Division")
+print(15 / 3)
+
+print("Modulo")
+print(7 % 3)
+
+print("Exponente")
+print(2 ** 3)
+
+print("Orden de procedencia para operaciones aritmetica")
+print("Parentesis -> Potencias -> Raices -> Multipilicacion -> Division -> Suma -> Resta")
+
+print(5 + 5 * 2)
---
+++ Variables.py
@@ -0,0 +1,17 @@
+numero = 3
+print(numero)
+
+numero1 = 5
+numero2 = 3
+
+print(numero1 + numero2)
+
+resultado = numero1 + numero2
+print(resultado)
+
+#Reglas del nombre de las variables
+#1) Las variables no pueden empezar por un número
+#2) Recomendacion: Escribir siempre en minúscula
+#3) Separar las palbras mediante un guión bajo (_)
+#4) No se pueden escribir espacio
+#5) No pueden contener caracteres especiales
